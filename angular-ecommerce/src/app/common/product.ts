2ec2779f73ab5a8db0b950e8294ac3b41537a1b2
KSanjeetTiwari <sanjeettiwari52@gmail.com>
2021-07-25 18:12:02 +0530

    First commit

---
+++ angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+
+   
+    id: number;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+
+     
+}
+
