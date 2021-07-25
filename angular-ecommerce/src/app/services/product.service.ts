c6515118a4b6eac6a0a795bff70310034b68717b
KSanjeetTiwari <sanjeettiwari52@gmail.com>
2021-07-25 19:50:06 +0530

    added rest call

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = "http://localhost:9090/products";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList():Observable<Product[]>{
+    console.log("calling rest")
+    return this.httpClient.get<Product[]>(this.baseUrl).pipe(map(response=>{
+      return response;
+    }
+      ));
+  }
+  
 }
